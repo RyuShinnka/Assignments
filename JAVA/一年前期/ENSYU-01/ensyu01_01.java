9ad9cfe4b0cb8e9b2aa6d0589f713a7130de8242
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:48:57 +0900

    Create ensyu01_01.java

---
+++ JAVA/一年前期/ENSYU-01/ensyu01_01.java
@@ -0,0 +1,13 @@
+public class ensyu01_01 {
+    public static void main(String[] args) {
+        // 数値を設定
+        int num1 = 82;
+        int num2 = 17;
+        // 和と差を計算
+        int sum = num1 + num2;
+        int sa = num1 - num2;
+        // 結果を表示
+        System.out.println("82 と 17 の和:" + sum);
+        System.out.println("82 と 17 の差:" + sa);
+    }
+}
