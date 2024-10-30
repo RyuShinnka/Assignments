da3ac4643314ff1167b28de4d99ebec84086e85b
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 11:34:07 +0900

    Create echo.php

---
+++ PHP/1003/echo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>hello</title>
+</head>
+<body>
+    <p>
+        <?php
+        print "Hello\n";
+        print "hello";
+        print "<div>hello print\n</div>";
+        echo "<div>","Hello echo!","</div>";
+        print "<h1>hello</h1>";
+        ?>
+    </p>
+</body>
+</html>
