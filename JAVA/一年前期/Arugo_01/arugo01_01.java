2a1375dc90a0623e6ade067dfdd0986dacdc79b2
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:21:03 +0900

    arugo01_01.java

---
+++ JAVA/一年前期/Arugo_01/arugo01_01.java
@@ -0,0 +1,29 @@
+/* ***さんのフローチャート */
+public class arugo01_01 {
+//	public static void main(String[] args) {
+//		int A = 5;
+//		int B = 10;
+//		int C = 15;
+//		int sum = A + B + C;
+//		int Avg = 3 / sum;
+//		System.out.println(sum);
+//		System.out.println(Avg);
+//	}
+	
+	//以下は自分のプログラミングした
+	
+//	問題１
+//	3回ゲームの点数を各変数に代入し、合計と平均を 計算します。
+//	合計と平均点を表示しましょう。
+	public static void main(String[] args) {
+		int s1 = 3;
+		int s2 = 9;
+		int s3 = 13;
+		int sum = s1 + s2 + s3;
+		int avg = sum / 3;
+		System.out.println("ゲームの点数の合計は："+sum+" , ゲームの点数の平均は："+avg);
+	}
+}
+
+
+
