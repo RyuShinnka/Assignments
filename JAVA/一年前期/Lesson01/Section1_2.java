55c15ff17eb1e09843da48ff2ab8c064a0c5f60e
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 11:01:57 +0900

    Add files via upload

---
+++ JAVA/一年前期/Lesson01/Section1_2.java
@@ -0,0 +1,13 @@
+/* 劉シンカ
+ * 0426
+ * Lesson01
+ */
+public class Section1_2 
+{
+
+	public static void main(String[] args) 
+	{
+		//\nを使って改行
+		System.out.println("TSB ITビジネス学科\n入学した劉シンカです。");
+	}
+}
---
+++ JAVA/一年前期/Lesson01/Section1_3.java
@@ -0,0 +1,12 @@
+/* 劉シンカ
+ * 0426
+ * Lesson01
+ */
+public class Section1_3 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("テキストは\\1,500です");
+	}
+}
