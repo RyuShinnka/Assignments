5acf1ddbcb91675da36a549341f3bfb682a68920
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:30:20 +0900

    List2_4.java

---
+++ JAVA/一年前期/Chapter02/List2_4.java
@@ -0,0 +1,31 @@
+/*
+ *  P-34 例題
+ *  変数の宣言
+ * 作成日：2024年4月26日
+ * 
+ */
+public class List2_4 
+{
+	public static void main(String[] args) 
+	{
+		int N;
+		N = 5;
+		//変数の定義
+		System.out.println("Nの値：" +N);
+		//P36
+		N = N+10;
+		System.out.println("Nの値：" +N);
+		//P39
+		double d;
+		d = 9.9;
+		char c;
+		c = 'あ';
+		byte b = 81;
+		boolean bool = true;
+		System.out.println("dの値は" +d+"です");
+		System.out.println("cの値は" +c+"です");
+		System.out.println("bの値は" +b+"です");
+		System.out.println("boolの値は" +bool+"です");
+		
+	}
+}
