7da9cd80071c223a2dcec2d5e6ed5cda35e0d105
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:41:43 +0900

    Create list03_01.java

---
+++ JAVA/一年前期/Chapter03/list03_01.java
@@ -0,0 +1,9 @@
+
+public class list03_01 {
+public static void main(String[] args) {
+	int age = 18; //ageという整数の変数を宣言する
+	if ( age < 20 ) {//変数ageの値を判断する、もし20より小さいときに
+		System.out.println("未成年ですよね！");//ここのコードを実行される
+	}
+}
+}
