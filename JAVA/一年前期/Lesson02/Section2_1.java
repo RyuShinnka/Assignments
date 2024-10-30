23ab9be5f6b77bbecf4ce30d353efdd399bfe623
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 11:03:03 +0900

    Create Section2_1.java

---
+++ JAVA/一年前期/Lesson02/Section2_1.java
@@ -0,0 +1,12 @@
+
+public class Section2_1 {
+	public static void main(String[] args) {
+		int i = 2008;
+		double dbl = 3.14;
+		char c = 'm';
+		System.out.println(i);
+		System.out.println(dbl);
+		System.out.println(c);
+		
+	}
+}
