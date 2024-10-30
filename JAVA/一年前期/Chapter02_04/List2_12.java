620fbd5c48e44feb4a7d82c28e95821579352fac
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:38:23 +0900

    Add files via upload

---
+++ JAVA/一年前期/Chapter02_04/List2_12.java
@@ -0,0 +1,15 @@
+//
+//P-53
+//
+//
+
+public class List2_12 {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		double c = a / b ;
+		System.out.println("cの値は：" + c);
+		double c1 = (double)a / (double)b ;
+		System.out.println("c1の値は：" + c1);
+	}
+}
---
+++ JAVA/一年前期/Chapter02_04/List2_14.java
@@ -0,0 +1,13 @@
+//
+//P-54
+//
+//
+
+public class List2_14 {
+	public static void main(String[] args) {
+		String message1 = "こんにちは。";
+		String message2 = "今日は良い天気ですね。";
+		String message3 = message1 + message2;
+		System.out.println(message3);
+	}
+}
