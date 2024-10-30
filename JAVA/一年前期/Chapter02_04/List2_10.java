f12ef9bee945e7909c71e9a37dcf2a2c3b0f51a1
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:38:06 +0900

    List2_10.java

---
+++ JAVA/一年前期/Chapter02_04/List2_10.java
@@ -0,0 +1,15 @@
+/*
+ *  P-50
+ * 
+ * 
+ */
+public class List2_10 {
+	public static void main(String[] args) {
+		double d = 9.8;
+		int i = (int) d;
+		// d = i;
+		System.out.println("dの値は：" + d);
+		System.out.println("iの値は：" + i);
+				
+	}
+}
