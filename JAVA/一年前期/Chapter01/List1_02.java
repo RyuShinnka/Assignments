3f7a36e3c7ccee104fd6298a02f94e3bda92fb29
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-30 09:28:27 +0900

    List1_02.java

---
+++ JAVA/一年前期/Chapter01/List1_02.java
@@ -0,0 +1,15 @@
+/*
+ *1-1JAVA言語のプログラム
+ * 練習：文字を画面に表示するプログラム
+ * 作成日：2024年4月19日
+ * 
+ */
+public class List1_02 
+{
+	public static void main(String[] args) 
+	{
+		//画面へ文字列を出力する
+		System.out.println("1-1JAVA言語に触れる");
+		System.out.println("プログラムの構成");
+	}
+}
