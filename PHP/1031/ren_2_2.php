a3fdfa97bd5e7bb9e0814a72d7a432ec956ee7e7
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-31 16:20:55 +0900

    Add files via upload

---
+++ PHP/1031/ren_2_2.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ren_02</title>
+    <style>
+        body {
+            background-color: #f5f5f5;
+            width: 1000px;
+            margin: 0 auto;
+        }
+
+        fieldset {
+            margin: 0 auto;
+            border: 3px solid #ccc;
+            padding: 10px;
+            margin-top: 20px;
+            width: 33%;
+        }
+    </style>
+</head>
+
+<body>
+    <fieldset>
+        <b>
+            <?php
+            print("①<br>");
+            $tarou = array("英語" => 90, "国語" => 85, "数学" => 70);
+            foreach ($tarou as $key => $value) {
+                echo "太郎の" . $key . "の点数は" . $value . "点です。<br>";
+            }
+            echo "<br><br>";
+
+            print("②<br>");
+            $kamoku = array("英語", "国語", "数学");
+            foreach ($kamoku as $key => $value) {
+                echo "太郎の" . $value . "の点数は" . $tarou[$value] . "点です。<br>";
+            }
+            echo "<br><br>"; 
+
+            print("③<br>");
+            $kuni = array("日本", "中国", "イギリス", "アメリカ合衆国", "カナダ");
+            $syuto = array("東京", "北京", "ロンドン", "ワシントン", "オタワ");
+            $world = array($kuni, $syuto);
+            // count()はjavaの.lengthと同じ使い方
+            for ($i = 0; $i < count($kuni); $i++) {
+                echo $kuni[$i] . "の首都は" . $syuto[$i] . "です。<br>";
+            }
+            ?>
+        </b>
+    </fieldset>
+</body>
+
+</html>
