a48f474cf8b45e4f8424cbb3f20bd10f938c1685
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-10-31 15:33:08 +0900

    Add files via upload

---
+++ PHP/1031/uru.php
@@ -0,0 +1,33 @@
+<?php
+$uru = isset($_POST['year']) ? (int)$_POST['year'] : 0;
+
+// うるう年のカウント
+$count = 0;
+
+// 1年から指定された年までループ
+for ($year = 1; $year <= 10000; $year++) {
+    // 入力された年を超えたらループ終了
+    if ($year > $uru) {
+        break;
+    }
+
+    // うるう年の判定
+    if ($year % 400 == 0 || ($year % 4 == 0 && $year % 100 != 0)) {
+        $count++;
+
+        // 400で割り切れる年は赤、それ以外は黒で表示
+        if ($year % 400 == 0) {
+            print "<span style='color: red;'>$year</span> ,";
+        } else {
+            print "<span style='color: black;'>$year</span> ,";
+        }
+        // 20年ごとに改行
+        if ($count % 20 == 0) {
+            print "<br>";
+        }
+    }
+}
+
+// 出力表示
+echo "<span>うるう年は: $count 回です</span>";
+?>
