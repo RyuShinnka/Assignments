f02ed5038810b9e9458e817fb1e2109e2cfad931
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-11-07 14:48:43 +0900

    Update sonyu.php
    fixed bugs

--- PHP/1107/sonyu.php
+++ PHP/1107/sonyu.php
@@ -7,12 +7,12 @@
 <body>
 <?php
 // require を使う
-require "4_yomikomi.php";
-print "4_yomikomi.phpの挿入・・・$a<br>";
+require "yomikomi.php";
+print "yomikomi.phpの挿入・・・$a<br>";
 
 // include を使う
-include "4_yomikomi2.php";
-print "4_yomikomi2.phpの挿入・・・$a<br>";
+include "yomikomi.php";
+print "yomikomi.phpの挿入・・・$a<br>";
 ?>
 </body>
 </html>
