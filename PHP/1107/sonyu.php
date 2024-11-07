97944337a3ae645e5f9a0f0e6ca1932d46b5d144
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-11-07 13:58:55 +0900

    Create sonyu.php

---
+++ PHP/1107/sonyu.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>sonyu</title>
+</head>
+<body>
+<?php
+// require を使う
+require "4_yomikomi.php";
+print "4_yomikomi.phpの挿入・・・$a<br>";
+
+// include を使う
+include "4_yomikomi2.php";
+print "4_yomikomi2.phpの挿入・・・$a<br>";
+?>
+</body>
+</html>
