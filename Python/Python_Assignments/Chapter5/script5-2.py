912f12d84ba9243d57c83db5d0c9183f7d110cbd
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-11-19 10:03:16 +0900

    Update script5-2.py

--- Python/Python_Assignments/Chapter5/script5-2.py
+++ Python/Python_Assignments/Chapter5/script5-2.py
@@ -1,5 +1,5 @@
 scores = [100, 64, 48, 83]
 print("scores ", scores)
-print("合計：", sum(scores))
-print("平均：", sum(scores)/len(scores))
+print("合計："+ str( scores[0] + scores[1] + scores[2] + scores[3]))
+print("平均："+ str( sum(scores)/len(scores)))
 
