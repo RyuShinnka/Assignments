cfb8b9a2a826231824ae1a43b4281cf9d68ca06d
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-11-29 14:48:43 +0900

    Add files via upload

---
+++ JAVA/一年後期/Chap05-ren/List5_ren01.java
@@ -0,0 +1,18 @@
+package chap05_ren;
+
+public class List5_ren01 {
+	public static void printInfo(Person p) {
+		System.out.println(p.name+"の年齢は"+p.age+"歳です");
+	}
+	public static void main(String[] args) {
+		Person a = new Person();
+		a.name = "高橋太郎";
+		a.age = 19;
+		Person b = new Person();
+		b.name = "小林花子";
+		b.age = 18;
+		
+		printInfo(a);
+		printInfo(b);
+	}
+}
