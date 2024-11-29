1173c42d840ec7ac79b8882185f720cc5af2624f
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-11-29 14:53:58 +0900

    Add files via upload

---
+++ JAVA/一年後期/Chap05-ren/List5_ren02.java
@@ -0,0 +1,22 @@
+package chap05_ren;
+
+public class List5_ren02 {
+	public static boolean ageCheck(Person p,int i) {
+		if(p.age > i) {
+			return true;
+		}else {
+			return false;
+		}
+	}
+	public static void main(String[] args) {
+		Person a = new Person();
+		a.name = "高橋太郎";
+		a.age = 19;
+		Person b = new Person();
+		b.name = "小林花子";
+		b.age = 18;
+		
+		boolean result=ageCheck(b,20);
+		System.out.println(b.name+"さんは20歳超えますか？"+result);
+	}
+}
