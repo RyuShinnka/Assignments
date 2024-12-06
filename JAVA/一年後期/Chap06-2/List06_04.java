bfca3afd7b2fa0758a465917e013401501ec4f83
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-12-06 14:02:49 +0900

    Create List06_04.java

---
+++ JAVA/一年後期/Chap06-2/List06_04.java
@@ -0,0 +1,22 @@
+class StudentCard{
+	int id;
+	String name;
+	
+	StudentCard(int id, String name){
+		this.id = id;
+		this.name = name;
+	}
+	
+	void printInfo() {
+		System.out.println("学籍番号："+ this.id);
+		System.out.println("氏名："+ this.name);
+	}
+}
+public class List06_04 {
+	public static void main(String[] args) {
+		StudentCard a = new StudentCard(1234, "鈴木太郎");
+		StudentCard b = new StudentCard(1235, "佐藤花子");
+		a.printInfo();
+		b.printInfo();
+	}
+}
