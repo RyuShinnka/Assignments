2ecb35e5ce337ee92cd3062455c31f72b3b358ac
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-12-06 12:20:10 +0900

    穴埋めvar

--- PHP/1205/keijiban_1.php
+++ PHP/1205/keijiban_1.php
@@ -1,104 +1,66 @@
 <?php
-    // データファイルの読み込み
-    $dataFile = 'keijiban.txt';
-    $ext = file_exists($dataFile);
-    $lines = $ext ? file($dataFile) : array();
-    $errMsg = "";
-    date_default_timezone_set('Asia/Tokyo');
+date_default_timezone_set('Asia/Tokyo');//tokyo 時間にする
+if(file_exists('keijiban_1.txt')){ //keijiban_1.txt が存在するなら
+    $f = file('keijiban_1.txt'); //配列$f に読み込み
+    $ln = explode(",", $f[0]); //データを,で分割し、最初のデータ(No)を$ln に代入
+    $no = sprintf("%03d", $ln[0]+1); //No に1を足し3桁に成型する
+}else{ // keijiban.txt が存在しないなら
+$f = array(); //データ配列だけつくる
+$no = "001"; //NO は 001 から始める
+}
+if($_SERVER["REQUEST_METHOD"]=="POST"){ //ボタンが押されたとき
+if(!$_POST['name']){ //名前未入力ならば
+$errMsg = '名前を入力してください<BR>'; // エラーメッセージを設定
+}
+elseif(!$_POST['free']){ //記事未入力ならば
+$errMsg = '記事を入力してください'; // エラーメッセージを設定
+}
+if(!$errMsg){ //エラーメッセージが設定されなかったら新規データを追加
+$free = preg_replace("/\n/","<BR>",$_POST['free']); //改行コードを<br>に置き換え
+$time = date("H:i:s"); //現在の時分秒を変数に入れる
+$data= array($no, $_POST['name'], $free, $time);//4つのデータ No 名前 メッセージ 時間を配列へ
+$datag=implode(",", $data); //$data の内容を,で結合
+array_unshift($f,$datag."\n");//\n を付けて配列$f の先頭へ$datag を入れる
+$fp = fopen('keijiban_1.txt', 'w'); //書き込み用でファイルを開く
+foreach($f as $line)
+    fwrite($fp,$line); //ファイルに書き込み
+fclose($fp); //閉じる
+header("Location:keijiban_1.php");//リロード時の再投稿を防ぐ。指定ファイルにリダイレクト
+exit;//終了
+}
+}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<head><title>掲示板</title></head>
+<body>
+<form method="post" action="keijiban_1.php">
+<h1>掲示板</h1>
+<?php
 
-    // 「書き込む」ボタンが押されたとき
-    if (isset($_POST['submit'])) {
-        // エラーチェック
-        if(empty($_POST['name'])){
-            $errMsg = "名前を入力してください。<br>";
-        }elseif(empty($_POST['free'])){
-            $errMsg .= '記事を入力してください';
-        }
-        // エラーがなければ新規データを追加
-        if(!$errMsg){
-            // タグなどを適用させない関数
-            function convert_str($str) {
-                $str = htmlspecialchars($str); // HTMLエスケープ
-                $str = str_replace("\r\n", "<br>", $str); // 改行を <br> に変換
-                $str = str_replace(["\r", "\n"], "<br>", $str); // 他の改行文字を変換
-                return $str;
-            }
-            
-            $ln = explode(",", $lines[0]);
-            $no = $ext ? sprintf("%03d", $ln[0]+1) : "001";
-            $name = convert_str($_POST['name']);
-            $free = convert_str($_POST['free']);
-            $time = date("Y/m/d H:i:s");
-
-            // 新規データをカンマ区切りテキストとし、配列の先頭に追加
-            $data = "$no,$name,$free,$time\n";
-            array_unshift($lines, $data);
-        }
-    }
-    // 「削除」ボタンが押されたとき
-    // 既存データとそれぞれ一致したものを探して探して削除
-    if (isset($_POST['delbtn']) && $ext) {
-        for($i = 0; $i<count($lines);$i++){
-            $ln = explode(",",$lines[$i]);
-            if($ln[0] == $_POST['no'] && $ln[3] == $_POST['Rdkey']){
-                array_splice($lines,$i,1);
-                break;
-            }
-        }
-    }
-
-    // 「書き込む」または「削除」ボタンが押されたとき
-    // 上記配列をファイルに書き込み
-    if(isset($_POST['submit']) || isset($_POST['delbtn'])){
-        $fk = fopen($dataFile,"w");
-        foreach($lines as $line)
-            fputs($fk,$line);
-        fclose($fk);
-    }
-    ?>
-    <!DOCTYPE html>
-    <html lang="ja">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>絵本の掲示板</title>
-        <link rel="stylesheet" href="keijiban.css">
-    </head>
-    <body>
-        <div id="wrapper">
-            <form method="post" action="keijiban_1.php">
-                <div class="center">
-                    <div id="title">絵本の掲示板</div>
-                </div>
-                <?php 
-                // エラーメッセージがあれば表示
-                if($errMsg){
-                    echo "<div id='errMsg'>".$errMsg."</div>";
-                }
-                ?>
-                <div id="edit_area">
-                    <p>
-                        名前:<input type="text" name="name" size="26">
-                    </p>
-                    <p>
-                        記事：<br><textarea name="free" ></textarea>
-                    </p>
-                    <p class="center">
-                        <input type="submit" name="submit" value="書き込む">
-                        <input type="reset" value="取り消す">
-                    </p>
-                </div>
-                <?php 
-                // ファイルから一行ずつ読み込みでーぶるにセットしていく
-                foreach($lines as $line){
-                    $ln = explode(",",$line);
-                    echo "<div><p class='entry_ID'>[No. ".$ln[0]." 名前: ".$ln[1]." &nbsp;"; 
-                    echo "書込み日付：".$ln[4]."</p>";
-                    echo "<p>".$ln[2]."</p>";
-                    echo "</div><hr>";
-                }
-                ?>
-            </form>
-        </div>
-    </body>
-    </html>
+if(@$errMsg){ //エラーメッセージが設定されたとき(@をつけておくとエラーをはかない)
+echo "<span style=color:red>".@$errMsg."</span>";
+}
+?>
+<table border = "1">
+<tr><td>名前:
+<input type="text" name="name" size="27">記事<br>
+<textarea name="free" cols="60" rows="8"></textarea><br><br>
+<input type="submit" name="submit" value="書き込む">
+<input type="reset" value="取り消す">
+</td></tr>
+</table><br><hr><br>
+<?php
+foreach ($f as $line) { //ファイルから一行ずつ読み込み
+    list($no, $name, $free, $time) = explode(",", $line); // ,(カンマ)で分割し、4つの変数に代入
+    print "<table border=1 width=500><tr>";
+    echo "<th>[No." . $no . "] 名前: " . $name . "</th>"; //No と名前を表示
+    echo "<th>書込み日付: " . $time . "</th>"; //日時表示
+    echo "</tr><tr>";
+    echo "<td align='left' colspan='2'>" . $free . "</td>"; //記事表示
+    echo "</tr></table><br>";
+}
+?>
+</form></body></html>
