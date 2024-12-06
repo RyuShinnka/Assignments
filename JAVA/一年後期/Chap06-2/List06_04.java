3fb056f27de90aea15bce2312a057bf6e53ddd6d
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-12-06 14:04:08 +0900

    Update List06_04.java

--- JAVA/一年後期/Chap06-2/List06_04.java
+++ JAVA/一年後期/Chap06-2/List06_04.java
@@ -1,22 +1,29 @@
-class StudentCard{
-	int id;
-	String name;
-	
-	StudentCard(int id, String name){
-		this.id = id;
-		this.name = name;
-	}
-	
-	void printInfo() {
-		System.out.println("学籍番号："+ this.id);
-		System.out.println("氏名："+ this.name);
-	}
+// 学生情報を保持するクラス
+class StudentCard {
+    int id; // 学籍番号
+    String name; // 氏名
+
+    // コンストラクタ
+    StudentCard(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    // 学生情報を表示するメソッド
+    void printInfo() {
+        System.out.println("学籍番号：" + this.id);
+        System.out.println("氏名：" + this.name);
+    }
 }
+
 public class List06_04 {
-	public static void main(String[] args) {
-		StudentCard a = new StudentCard(1234, "鈴木太郎");
-		StudentCard b = new StudentCard(1235, "佐藤花子");
-		a.printInfo();
-		b.printInfo();
-	}
+    public static void main(String[] args) {
+        // 学生情報を作成
+        StudentCard a = new StudentCard(1234, "鈴木太郎");
+        StudentCard b = new StudentCard(1235, "佐藤花子");
+
+        // 学生情報を表示
+        a.printInfo();
+        b.printInfo();
+    }
 }
