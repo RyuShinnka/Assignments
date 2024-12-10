6f0edcaa3ca68ae6a41a71c176aef630b8adea51
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2024-12-10 11:50:19 +0900

    コメント追加

--- Python/Python_Assignments/Chapter7/kan-mon1.py
+++ Python/Python_Assignments/Chapter7/kan-mon1.py
@@ -1,6 +1,11 @@
+# 指定された回数分、授業回数を表示する関数
 def show(times):
-    for i in range(times):
-        print('Pythonの授業は', i+1, '回目です')
+    for i in range(times):  # 0からtimes-1まで繰り返し
+        print('Pythonの授業は', i+1, '回目です')  # 授業回数を表示
+
+# ---------メイン処理---------
 
 times = int(input('Pythonの授業の回数を入力ー＞'))
+
+# 入力された回数分、授業回数を表示する関数を呼び出す
 show(times)
