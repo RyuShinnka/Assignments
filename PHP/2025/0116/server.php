8320f0db0111b9381fafd346941bf05c272aa54b
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-01-16 14:02:15 +0900

    Add files via upload

---
+++ PHP/2025/0116/server.php
@@ -0,0 +1,18 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    foreach ($_SERVER as $key => $value) {
+        echo "<b>$key:</b> $value<br>";
+    }
+}
+?>
+<html>
+<head>
+    <title>server</title>
+</head>
+<body>
+    確認ボタンを押して $_SERVER の内容を表示してみましょう<br>
+    <form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="POST" name="form1">
+        <input type="submit" name="sub1" value="確認">
+    </form>
+</body>
+</html>
