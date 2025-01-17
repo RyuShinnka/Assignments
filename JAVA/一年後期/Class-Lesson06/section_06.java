f185f65407af9d8e9541bcf6381cab03497b99b9
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-01-17 15:59:43 +0900

    Update section_06.java

--- JAVA/一年後期/Class-Lesson06/section_06.java
+++ JAVA/一年後期/Class-Lesson06/section_06.java
@@ -31,12 +31,7 @@ class PeopleCard {
 
     // 会員情報を表示
     void PrintMethod() {
-        String genderText;
-        if (this.gender == 1) {
-            genderText = "男子";
-        } else {
-            genderText = "女子";
-        }
+        String genderText = (this.gender == 1) ? "男子" : "女子";
         System.out.println();
         System.out.println("―――――――――");
         System.out.println("名前：" + this.name);
@@ -49,84 +44,42 @@ class PeopleCard {
 public class section_06 {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+        
+        // 会員情報を管理する配列を作成
+        PeopleCard[] peopleCards = new PeopleCard[3];
 
-        // 一人目の情報を入力
-        System.out.print("一人目の名前を入力＝＞");
-        String nameP1 = s.next();
-        System.out.print("一人目の会員番号（1000～3999）を入力＝＞");
-        int memberNumP1 = s.nextInt();
-        System.out.print("一人目の性別（男性：1、女性：2）を入力＝＞");
-        int genderP1 = s.nextInt();
-        System.out.print("一人目の年齢を入力＝＞");
-        int ageP1 = s.nextInt();
-        PeopleCard p1 = new PeopleCard(nameP1, memberNumP1, genderP1, ageP1);
+        // 3人分の会員情報を入力
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + "人目の名前を入力＝＞");
+            String name = s.next();
+            System.out.print((i + 1) + "人目の会員番号（1000～3999）を入力＝＞");
+            int memberNum = s.nextInt();
+            System.out.print((i + 1) + "人目の性別（男性：1、女性：2）を入力＝＞");
+            int gender = s.nextInt();
+            System.out.print((i + 1) + "人目の年齢を入力＝＞");
+            int age = s.nextInt();
 
-        // 会員番号が範囲外の場合は再入力
-        while (true) {
-            if (p1.CheckNoMethod() == false) {
+            // 会員情報を作成
+            PeopleCard person = new PeopleCard(name, memberNum, gender, age);
+            // 会員番号が範囲外の場合は再入力
+            while (!person.CheckNoMethod()) {
                 System.out.println("会員番号入力エラー、もう一度正しく入力してください。");
-                System.out.print("一人目の会員番号（1000～3999）を入力＝＞");
-                memberNumP1 = s.nextInt();
-                p1.ChangeMethod(memberNumP1);
-            } else {
-                break;
+                System.out.print((i + 1) + "人目の会員番号（1000～3999）を入力＝＞");
+                memberNum = s.nextInt();
+                person.ChangeMethod(memberNum);
             }
+
+            // 配列に格納
+            peopleCards[i] = person;
+
+            System.out.println("ーーーーーーーーーー");
         }
 
-        System.out.println("ーーーーーーーーーー");
-
-        // 二人目の情報を入力
-        System.out.print("二人目の名前を入力＝＞");
-        String nameP2 = s.next();
-        System.out.print("二人目の会員番号（1000～3999）を入力＝＞");
-        int memberNumP2 = s.nextInt();
-        System.out.print("二人目の性別（男性：1、女性：2）を入力＝＞");
-        int genderP2 = s.nextInt();
-        System.out.print("二人目の年齢を入力＝＞");
-        int ageP2 = s.nextInt();
-        PeopleCard p2 = new PeopleCard(nameP2, memberNumP2, genderP2, ageP2);
-
-        // 会員番号が範囲外の場合は再入力
-        while (true) {
-            if (p2.CheckNoMethod() == false) {
-                System.out.println("会員番号入力エラー、もう一度正しく入力してください。");
-                System.out.print("二人目の会員番号（1000～3999）を入力＝＞");
-                memberNumP2 = s.nextInt();
-                p2.ChangeMethod(memberNumP2);
-            } else {
-                break;
-            }
+     // 各会員の情報を表示
+        for (int i = 0; i < peopleCards.length; i++) {
+            peopleCards[i].PrintMethod();
         }
 
-        System.out.println("ーーーーーーーーーー");
-
-        // 三人目の情報を入力
-        System.out.print("三人目の名前を入力＝＞");
-        String nameP3 = s.next();
-        System.out.print("三人目の会員番号（1000～3999）を入力＝＞");
-        int memberNumP3 = s.nextInt();
-        System.out.print("三人目の性別（男性：1、女性：2）を入力＝＞");
-        int genderP3 = s.nextInt();
-        System.out.print("三人目の年齢を入力＝＞");
-        int ageP3 = s.nextInt();
-        PeopleCard p3 = new PeopleCard(nameP3, memberNumP3, genderP3, ageP3);
-
-        // 会員番号が範囲外の場合は再入力
-        while (true) {
-            if (p3.CheckNoMethod() == false) {
-                System.out.println("会員番号入力エラー、もう一度正しく入力してください。");
-                System.out.print("三人目の会員番号（1000～3999）を入力＝＞");
-                memberNumP3 = s.nextInt();
-                p3.ChangeMethod(memberNumP3);
-            } else {
-                break;
-            }
-        }
-
-        // 各会員の情報を表示
-        p1.PrintMethod();
-        p2.PrintMethod();
-        p3.PrintMethod();
         s.close();
     }
 }
