e2e796a227a1271db4b7cbf1410e1ef876b29b55
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-02-06 15:31:19 +0900

    Update 5_count_g.php

--- PHP/1121/5_count_g.php
+++ PHP/1121/5_count_g.php
@@ -1,30 +1,33 @@
 <?php
-
-$count_file = "count.txt";
-
-$count = 0;
+echo '<p>アクセスカウンター</p>';
 
 // カウントファイルが存在するかチェック
-if (file_exists($count_file)) {
+if (file_exists("count.txt")) {
     // カウントファイルを読み取りモードで開く
-    $fp = fopen($count_file, "r");
-    $count = (int)fread($fp, filesize($count_file));
-    fclose($fp);
-    $count++;
+    $fp = @fopen("count.txt", "r") or die("file Error");
+    
+    $count = fgets($fp);
+    $newNum = sprintf("%05d", $count);
+    // カウント結果を画面に表示
+    for($i =0; $i <strlen($newNum); $i++){
+        $suu = substr($newNum, $i, 1);
+        echo "<img src='counter/$suu.png' alt='$suu'>";
+    }
+    echo "<br>人目の訪問者です。";
+
+    fclose($fp);// ファイルを閉じる
+    
 }
+$count++; // カウントを1増やす
 
 // カウントファイルを新規書き込みモードで開く
-$fp = fopen($count_file, "w");
-fwrite($fp, $count);
-fclose($fp);
 
-$newNum = sprintf("%05d", $count);
+$fh = @fopen("count.txt", "w") or die("Error\n");
+fputs($fh, $count);   # ファイルに書き込み
+fclose($fh); # ファイルを閉じる
 
-echo '<p>アクセスカウンター</p>';
-// カウント結果を画面に表示
-for($i =0; $i <strlen($newNum); $i++){
-    $suu = substr($newNum, $i, 1);
-    echo "<img src='counter/$suu.png' alt='$suu'>";
-}
-echo "<br>人目の訪問者です。";
+
+
+
+
 ?>
