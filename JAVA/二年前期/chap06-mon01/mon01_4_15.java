d57cfda4ea076688cb6f25bb3a382f13fe2a043f
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-15 16:22:21 +0900

    Create mon01_4_15.java

---
+++ JAVA/二年前期/chap06-mon01/mon01_4_15.java
@@ -0,0 +1,13 @@
+
+public class mon01_4_15 {
+	public static void main(String[] args) {
+		Student student1 = new Student(1009,"アキラ");
+		student1.printInfo();
+		
+		Student student2 = new Student(1019,"キアラ");
+		student2.printInfo();
+		
+		Student student3 = new Student(1039,"キラア");
+		student3.printInfo();
+	}
+}
