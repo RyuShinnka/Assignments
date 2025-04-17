80ad63b6d0655b6ce79214de9321d0189c47f790
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-17 10:37:08 +0900

    Update chap02_01.java

--- JAVA/2年前期JAVA基礎（選択）/Chapter02/chap02_01.java
+++ JAVA/2年前期JAVA基礎（選択）/Chapter02/chap02_01.java
@@ -1,16 +1,23 @@
+import java.util.Scanner;
 public class chap02_01 {
     public static void main(String[] args) {
-        // 三角形の底辺と高さを定義
-        double base = 3.5;
-        double height = 5.6;
+    	Scanner s = new Scanner(System.in);
+        // 三角形の底辺と高さを入力
+    	System.out.print("底辺を入力＝＞");
+        double base = s.nextDouble();
+        System.out.print("高さを入力＝＞");
+        double height = s.nextDouble();
 
-        // 三角形の面積を計算（小数）
+        // 三角形の面積を計算
         double result = base * height / 2;
 
-        // 小数を整数に変換（キャスト）
+        // 小数を整数に変換
         int intResult = (int) result;
 
         // 結果を表示
-        System.out.println("三角形の面積＝" + intResult + "cm²");
+        System.out.print("三角形の面積は");
+        System.out.println(intResult + "cm²");
+        
+        s.close();
     }
 }
