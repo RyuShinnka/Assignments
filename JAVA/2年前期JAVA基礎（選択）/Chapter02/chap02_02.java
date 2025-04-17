95e7e2feb5269227e1b1503835bc7f4d6ece66ed
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-17 11:51:44 +0900

    Update chap02_02.java

--- JAVA/2年前期JAVA基礎（選択）/Chapter02/chap02_02.java
+++ JAVA/2年前期JAVA基礎（選択）/Chapter02/chap02_02.java
@@ -1,6 +1,11 @@
 import java.util.Scanner;
 
 public class chap02_02 {
+    // BMIに基づいて体型を判定するメソッド
+    public static void hantei(int bmi) {
+        
+    }
+
     public static void main(String[] args) {
         // Scannerを使用してユーザーから入力を受け取る
         Scanner s = new Scanner(System.in);
@@ -17,9 +22,18 @@ public class chap02_02 {
         double BMI = weight / (height * height);
 
         // 計算結果を整数にキャストして表示
-        int intBMI = (int)BMI;
+        int intBMI = (int) BMI;
         System.out.print("あなたのBMIは");
-        System.out.println(intBMI + "です。");
+        System.out.print(intBMI + "です。");
+
+        // BMIに基づいて体型を判定して表示
+        if (intBMI > 27) {
+            System.out.println("肥満型ですね");
+        } else if (intBMI > 19) {
+            System.out.println("普通型ですね");
+        } else {
+            System.out.println("やせ型ですね");
+        }
         
         // Scannerを閉じる
         s.close();
