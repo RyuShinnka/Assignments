6f83232d464b4d0d86b47cd7373a038e6dae8416
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-18 15:05:38 +0900

    Add files via upload

--- Python/二年前期/Assignments(練習問題）/script8-2.py
+++ Python/二年前期/Assignments(練習問題）/script8-2.py
@@ -12,28 +12,27 @@ class Student:
         print('数学の点数：', self.math)
         print('英語の点数：', self.english)
         print('国語の点数：', self.japanese)
+        print('合計点：', self.get_total_score())
+        print('平均点：', self.get_averate_score())
+    
+    def get_total_score(self):
+        # 合計点を計算
+        return self.math + self.english + self.japanese
+    
+    def get_averate_score(self):
+        # 平均点を計算（小数第5位まで）
+        total_score = self.get_total_score()
+        return round(total_score / 3, 5)
 
-
-# 1人目の生徒情報を入力
+#生徒情報を入力
 stu1 = input('生徒名を入力してください-> ')
 math1 = int(input('数学の点数を入力してください-> '))
 english1 = int(input('英語の点数を入力してください-> '))
 japanese1 = int(input('国語の点数を入力してください-> '))
 print()
 
-# 2人目の生徒情報を入力
-stu2 = input('生徒名を入力してください-> ')
-math2 = int(input('数学の点数を入力してください-> '))
-english2 = int(input('英語の点数を入力してください-> '))
-japanese2 = int(input('国語の点数を入力してください-> '))
-print()
-
 # 生徒オブジェクトを作成
 student1 = Student(stu1, math1, english1, japanese1)
-student2 = Student(stu2, math2, english2, japanese2)
 
 # 表示
-print("＜生徒１＞")
 student1.show_detail()
-print("＜生徒２＞")
-student2.show_detail()
---
+++ Python/二年前期/Assignments(練習問題）/script8-3.py
@@ -0,0 +1,43 @@
+class Student:
+    def __init__(self, name, math, english, japanese):
+        # 名前と各教科の点数を初期化
+        self.name = name
+        self.math = math
+        self.english = english
+        self.japanese = japanese
+
+    def show_detail(self):
+        # 生徒の情報を表示
+        print('生徒名：', self.name)
+        print('数学の点数：', self.math)
+        print('英語の点数：', self.english)
+        print('国語の点数：', self.japanese)
+    
+    def get_total_score(self):
+        # 合計点を取得
+        return self.math + self.english + self.japanese
+    
+    def get_averate_score(self):
+        # 平均点を取得（小数第5位まで）
+        total_score = self.get_total_score()
+        return round(total_score / 3, 5)
+
+
+# 生徒リストを作成
+students = [Student('佐藤', 100, 40, 65), Student('丸山', 64, 98, 79),
+            Student('三村', 48, 87, 92), Student('古川', 83, 81, 74)]
+
+# 検索する生徒名を入力
+stu1 = input('生徒名を入力してください-> ')
+
+check = True
+for x in students:
+    # 生徒が見つかったら情報を表示
+    if stu1 == x.name:
+        x.show_detail()
+        check = False
+        break
+
+# 見つからなかった場合のメッセージ
+if check:
+    print('存在しません。')
