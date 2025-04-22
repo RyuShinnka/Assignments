720c1c3beb80b6cf64906356857917e85b7420fc
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-22 15:49:04 +0900

    Create List06_03.java

---
+++ JAVA/二年前期/chap06-6-01/List06_03.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GakuseiMain {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        GakuseiJoho[] gakuseiList = new GakuseiJoho[3]; // 3人分
+
+        for (int i = 0; i < 3; i++) {
+            System.out.print((i + 1) + "人目の学籍番号を入力＝＞");
+            int bangou = s.nextInt();
+
+            System.out.print((i + 1) + "人目の氏名を入力＝＞");
+            String namae = s.next();
+
+            System.out.print((i + 1) + "人目の成績点を入力＝＞");
+            int seiseki = s.nextInt();
+
+            // 学生情報を作成して配列に格納
+            gakuseiList[i] = new GakuseiJoho(bangou, namae, seiseki);
+            gakuseiList[i].goukeiNiKasaneru();
+        }
+
+        // 各学生の情報を表示
+        for (int i = 0; i < 3; i++) {
+            System.out.println("★" + (i + 1) + "人目の情報");
+            gakuseiList[i].johoHyouji();
+            System.out.println();
+        }
+
+        // 合計表示
+        System.out.println("三人の成績点の合計は：" + GakuseiJoho.seisekiGoukei);
+
+        s.close();
+    }
+}
