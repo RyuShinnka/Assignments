9ee2c748e4d9f52a97531439245782c161fd6f64
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-04-22 15:48:37 +0900

    Create StudentCard.java

---
+++ JAVA/二年前期/chap06-6-01/StudentCard.java
@@ -0,0 +1,30 @@
+// 学生情報クラス
+class GakuseiJoho {
+    int gakusekiBangou; // 学籍番号
+    String namae;       // 氏名
+    int bangou;         // 自動番号
+    int seiseki;        // 成績点
+    static int bangouCount = 0; // 番号カウンター
+    static int seisekiGoukei = 0; // 成績点の合計
+
+    // コンストラクタ
+    GakuseiJoho(int gakusekiBangou, String namae, int seiseki) {
+        this.gakusekiBangou = gakusekiBangou;
+        this.namae = namae;
+        this.seiseki = seiseki;
+        this.bangou = GakuseiJoho.bangouCount++;
+    }
+
+    // 学生情報を表示するメソッド
+    void johoHyouji() {
+        System.out.println("番号：" + this.bangou);
+        System.out.println("学籍番号：" + this.gakusekiBangou);
+        System.out.println("氏名　　：" + this.namae);
+        System.out.println("成績点　：" + this.seiseki);
+    }
+
+    // 成績点を合計に加えるメソッド
+    void goukeiNiKasaneru() {
+        GakuseiJoho.seisekiGoukei += this.seiseki;
+    }
+}
