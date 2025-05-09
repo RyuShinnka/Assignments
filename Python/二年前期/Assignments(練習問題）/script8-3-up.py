759cdeaf2e2dbd27df179b6fbc2e013835a729be
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-05-09 15:27:51 +0900

    Update script8-3-up.py

--- Python/二年前期/Assignments(練習問題）/script8-3-up.py
+++ Python/二年前期/Assignments(練習問題）/script8-3-up.py
@@ -12,16 +12,6 @@ class Student:
         print('数学の点数：', self.math)
         print('英語の点数：', self.english)
         print('国語の点数：', self.japanese)
-    
-    def get_total_score(self):
-        # 合計点を取得
-        return self.math + self.english + self.japanese
-    
-    def get_averate_score(self):
-        # 平均点を取得（小数第5位まで）
-        total_score = self.get_total_score()
-        return round(total_score / 3, 5)
-
 
 """
 ーーー メイン処理 ーーー
@@ -30,24 +20,15 @@ class Student:
 # 生徒リストを作成
 students = []
 
+# students = [Student('佐藤', 100, 40, 65), Student('丸山', 64, 98, 79),
+#             Student('三村', 48, 87, 92), Student('古川', 83, 81, 74)]
+
 for i in range(1, 6):
     name = input(str(i) +"人目の生徒の名前を入力＝＞")
     math = int(input(name + "さんの数学の点数を入力＝＞"))
     eng = int(input(name + "さんの英語の点数を入力＝＞"))
     ja = int(input(name + "さんの国語の点数を入力＝＞"))
     students.append(Student(name, math, eng, ja))
-
-
-
-
-
-# students = [Student('佐藤', 100, 40, 65), Student('丸山', 64, 98, 79),
-#             Student('三村', 48, 87, 92), Student('古川', 83, 81, 74)]
-
-
-
-
-
 
 # 検索する生徒名を入力
 stu1 = input('検索する生徒名を入力してください-> ')
@@ -59,7 +40,6 @@ for x in students:
         x.show_detail()
         check = False
         break
-
 
 # 見つからなかった場合のメッセージ
 if check:
