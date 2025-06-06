adb08c90eddb89a7648372deba426183841f64b7
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-06-06 15:41:28 +0900

    20250606の課題
    Chapter11の課題２

---
+++ Python/二年前期/Assignments(練習問題）/Chapter11/Fail-01/Fail-Mon-01.py
@@ -0,0 +1,12 @@
+txt = '''
+柴田,100,40,65
+古川,83,81,74'''
+f = open("Fail-01/students3.txt", "a", encoding="utf-8")
+f.write(txt)
+f.close()
+
+print("追加されたstudent3.txtのテキストデータを読み込み表示しましょう")
+f = open("Fail-01/students3.txt", encoding="utf-8")
+lines = f.readlines()
+print(lines)
+f.close()
