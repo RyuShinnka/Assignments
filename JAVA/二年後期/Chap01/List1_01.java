6adbafc0e0725a9689ab8d62dec3638f69c248e8
Ryu shinka <167270333+RyuShinnka@users.noreply.github.com>
2025-09-02 14:18:10 +0900

    Create List1_01.java
    Randomクラス

---
+++ JAVA/二年後期/Chap01/List1_01.java
@@ -0,0 +1,9 @@
+// P-8 
+import java.util.Random;
+public class List1_01 {
+	public static void main(String [] args) {
+		Random rand = new Random();
+//		0 ~ 1 の間のランダムな値を出力
+		System.out.println(rand.nextDouble());
+	}
+}
